f61d37578e8515a445b0e16f71afeeff8e27a185
Bola Ajose <bolaa.ajose@gmail.com>
2020-03-21 10:46:08 +0000

    styling tile content

--- src/components/Tiles/Tiles.js
+++ src/components/Tiles/Tiles.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import SearchResults from '../SearchResults/SearchResults';
+import './Tiles.css'
 
-const Tiles = ({ dailyData, overallData}) => {
+const Tiles = ({ dailyData }) => {
 
     return (
-    <div className="Tiles">
-       {dailyData && dailyData.map(data => {
-            return <SearchResults 
-            key={data.dt}
-            maxTemp={data.main.temp_max}
-            temperature={data.main.temp}
-            minTemp={data.main.temp_min}/>
-        })}
-        {/* <SearchResults error={dailyData.error} /> */}
+    <div className="container">
+        <div className="row justify-content-center">
+        {dailyData && dailyData.map(data => {
+                return <SearchResults 
+                key={data.dt}
+                maxTemp={data.main.temp_max}
+                temperature={data.main.temp}
+                minTemp={data.main.temp_min}/>
+            })}
+            {/* <SearchResults error={dailyData.error} /> */}
+        </div>
     </div>
     )
 }
