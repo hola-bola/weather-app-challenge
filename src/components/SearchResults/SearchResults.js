73162d39c24ad0aeb4d261541f49ef298349ebf9
Bola Ajose <bolaa.ajose@gmail.com>
2020-03-21 10:46:08 +0000

    styling tile content

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -6,17 +6,13 @@ const SearchResults = ({temperature, minTemp, maxTemp,condition, error }) => {
    
  
     return (
-        <div className="container">
-            <div className="row">
-                {temperature && <div className="tile col-lg-3  bg-light">
-                    {maxTemp && <p className="sup"> {Math.round(maxTemp)}<sup>°C</sup></p>}
-                    {temperature && <p className="temp">{Math.round(temperature)}<sup>°C</sup></p>}
-                    {minTemp && <p className="sub"> {Math.round(minTemp)}<sup>°C</sup></p>}
-                    {condition && <img src={condition} alt="condition-icon"/>}
-                </div>}
+            <div className="tile col-lg-3  bg-light">
+                {maxTemp && <p className="sup"> {Math.round(maxTemp)}<sup>°C</sup></p>}
+                {temperature && <p className="temp">{Math.round(temperature)}<sup>°C</sup></p>}
+                {minTemp && <p className="sub"> {Math.round(minTemp)}<sup>°C</sup></p>}
+                {condition && <img src={condition} alt="condition-icon"/>}
                 {error && <p className="error">{error}</p>}
             </div>
-        </div>
     )
 }
 
