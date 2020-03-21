f260fa84547dc0fd9f6111f70788c5cf62057447
Bola Ajose <bolaa.ajose@gmail.com>
2020-03-21 18:20:02 +0000

    Tile component updated with condition image

--- src/components/TileContainer/TileContainer.js
+++ src/components/TileContainer/TileContainer.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Tile from '../Tile/Tile';
+import './TileContainer.css'
 
-const TileContainer = ({ dailyData }) => {
+const TileContainer = ({ dailyData, error }) => {
 
     return (
     <div className="container">
         <div className="row justify-content-center">
-        {dailyData && dailyData.map(data => {
-                return <Tile 
-                key={data.dt}
-                maxTemp={data.main.temp_max}
-                temperature={data.main.temp}
-                minTemp={data.main.temp_min}/>
-            })}
-            {/* <Tile error={dailyData.error} /> */}
+            {error && <p className="error">{error}</p>}
+            {dailyData && dailyData.map(data => {
+                    return <Tile 
+                    key={data.dt}
+                    maxTemp={data.main.temp_max}
+                    temperature={data.main.temp}
+                    minTemp={data.main.temp_min}
+                    condition={data.weather[0].main}/>
+                })}
         </div>
     </div>
     )
---
+++ src/icons.js
@@ -0,0 +1,10 @@
+import clear from './Weather_Icons/PNG/clear.png';
+import clouds from './Weather_Icons/PNG/clouds.png';
+import drizzle from './Weather_Icons/PNG/drizzle.png';
+import rain from './Weather_Icons/PNG/rain.png';
+import snow from './Weather_Icons/PNG/snow.png';
+import sunny from './Weather_Icons/PNG/sunny.png';
+import thunderstorms from './Weather_Icons/PNG/thunderstorms.png';
+
+
+export { clear, clouds, drizzle, rain, snow, sunny, thunderstorms };
